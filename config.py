46369d7adec3495ba21a62f6730c66bd5e124905
nguyenvanbao201 <hunghackgamek9@gmail.com>
2026-07-04 07:35:33 +0700

    Delete config.py

--- config.py
+++
@@ -1,8 +0,0 @@
-# Token bot Telegram
-TOKEN = "8951792957:AAFxJOd9jwDEjP9NArZEyefzJ4afWKdED9Y"
-
-# Link4m API Token
-LINK4M_TOKEN = "6a2391ae3f35952ad60cb3cf"
-
-# URL Render của bạn
-BASE_URL = "https://vbtool.onrender.com"
